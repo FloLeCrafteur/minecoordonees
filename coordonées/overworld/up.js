2d611117378c1bab62ea52d6e39e7353c6385349
FloLeCrafteur <105410821+FloLeCrafteur@users.noreply.github.com>
2024-05-26 01:18:27 +0200

    Delete coordonées directory

--- coordonées/overworld/up.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const scrollButton = document.getElementById("upArrow");
-
-    scrollButton.addEventListener("click", function () {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    });
-});
